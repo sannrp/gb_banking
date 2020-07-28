3f8310183c4fadf8f3481fd92f802e2eb7de5cf3
Spiky <mrmrmtarr@gmail.com>
2020-07-28 19:23:09 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,30 +3,30 @@ Config              = {}
 Config.HelpText1 = "Naciśnij ~INPUT_PICKUP~ aby skorzystać z terminalu bankowego ~b~"
 Config.HelpText2 = "Naciśnij ~INPUT_PICKUP~ aby skorzystać z bankomatu ~b~"
 
-Config.Logged = "Zalogowano"
-Config.CurrentlyLogged = "Ktoś już jest zalogowany na tym koncie"
-Config.WrongPassword = "Nieprawidłowe hasło"
+Config.Logged = "Logged In"
+Config.CurrentlyLogged = "Someone is already logged in to this account"
+Config.WrongPassword = "Incorrect Password"
 
-Config.PINchanged = "Zmieniono kod PIN"
+Config.PINchanged = "You changed your Pin"
 
-Config.NotEnoughMoney = "Nie posiadasz tyle pieniędzy"
-Config.NewCreditCard = "Wygenerowano nową kartę kredytową"
+Config.NotEnoughMoney = "You do not have enough money"
+Config.NewCreditCard = "You got a new credit card"
 
-Config.PasswordChanged = "Zmieniono hasło"
-Config.WrongBackup = "Niepoprawny kod zapasowy"
+Config.PasswordChanged = "Password Changed"
+Config.WrongBackup = "Incorrect Backup Pin"
 
-Config.ArleadyHaveAccount = "Posiadasz już konto"
-Config.LoginInUse = "Login jest już zajęty"
-Config.AccountCreated = "Pomyślnie utworzono konto"
+Config.ArleadyHaveAccount = "You already have an account with us"
+Config.LoginInUse = "This login is already taken"
+Config.AccountCreated = "Your account has been created"
 
-Config.WrongAmount = "Nieprawidłowa kwota"
-Config.MoneyDeposit = "Wpłacono pieniądze"
-Config.MoneyWithdraw = "Wypłacono pieniądze"
+Config.WrongAmount = "Invalid Amount"
+Config.MoneyDeposit = "Money Deposited"
+Config.MoneyWithdraw = "Money Withdraw"
 
-Config.TransferToMe = "Nie możesz przelać pieniędzy do samego siebie"
-Config.TransferDone = "Przelew wykonany"
+Config.TransferToMe = "You cannot transfer money to yourself"
+Config.TransferDone = "Transfer Complete"
 
-Config.NullValues = "Login/Hasło/Kod zapasowy nie może być pusty"
-Config.BackupCodeLenght = "Kod zapasowy nie może być dłuższy niż 4 cyfry"
+Config.NullValues = "Login/Password/Backup code cannot be empty"
+Config.BackupCodeLenght = "The backup code cannot be longer than 4 digits"
 
-Config.ChangedMainAccount = "Zmieniłeś główne konto"
+Config.ChangedMainAccount = "You changed your main account"
